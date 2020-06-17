44f0603ba29f80732488cfaf48c551492b4f4a47
Jun <kjunn2000@gmail.com>
2020-06-17 13:38:39 +0800

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.addFirst(233);
+        list.addFirst(142);
+        list.addLast(213);
+        list.removeLast();
+        list.removeFirst();
+
+
+    }
+
+}
